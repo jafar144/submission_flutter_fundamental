aaa9678577baca36b87af762a3b9fba1512635a6
Ja'far <ajib.aiwa@gmail.com>
2024-09-30 20:53:36 +0700

    Cleaning function pengangguran

--- lib/model/restaurant_response.dart
+++ lib/model/restaurant_response.dart
@@ -3,9 +3,6 @@ import 'dart:convert';
 RestaurantResponse restaurantResponseFromJson(String str) =>
     RestaurantResponse.fromJson(json.decode(str));
 
-String restaurantResponseToJson(RestaurantResponse data) =>
-    json.encode(data.toJson());
-
 class RestaurantResponse {
   List<Restaurant> restaurants;
 
@@ -18,10 +15,6 @@ class RestaurantResponse {
         restaurants: List<Restaurant>.from(
             json["restaurants"].map((x) => Restaurant.fromJson(x))),
       );
-
-  Map<String, dynamic> toJson() => {
-        "restaurants": List<dynamic>.from(restaurants.map((x) => x.toJson())),
-      };
 }
 
 class Restaurant {
@@ -52,16 +45,6 @@ class Restaurant {
         rating: json["rating"]?.toDouble(),
         menus: Menus.fromJson(json["menus"]),
       );
-
-  Map<String, dynamic> toJson() => {
-        "id": id,
-        "name": name,
-        "description": description,
-        "pictureId": pictureId,
-        "city": city,
-        "rating": rating,
-        "menus": menus.toJson(),
-      };
 }
 
 class Menus {
@@ -77,11 +60,6 @@ class Menus {
         foods: List<Drink>.from(json["foods"].map((x) => Drink.fromJson(x))),
         drinks: List<Drink>.from(json["drinks"].map((x) => Drink.fromJson(x))),
       );
-
-  Map<String, dynamic> toJson() => {
-        "foods": List<dynamic>.from(foods.map((x) => x.toJson())),
-        "drinks": List<dynamic>.from(drinks.map((x) => x.toJson())),
-      };
 }
 
 class Drink {
@@ -94,8 +72,4 @@ class Drink {
   factory Drink.fromJson(Map<String, dynamic> json) => Drink(
         name: json["name"],
       );
-
-  Map<String, dynamic> toJson() => {
-        "name": name,
-      };
 }
